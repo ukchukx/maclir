77c59a00c12995a3a0d33372678c53cb1a0b518e
Uk <ukchukundah@gmail.com>
2017-12-26 19:52:14 +0100

    Add check_origin to transport config

--- lib/maclir_web/channels/user_socket.ex
+++ lib/maclir_web/channels/user_socket.ex
@@ -11,7 +11,8 @@ defmodule MacLirWeb.UserSocket do
   channel "user_presence:*", MacLirWeb.UserChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
